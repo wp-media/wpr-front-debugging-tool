23d2141cc0e01a245865687ce543e467b8459ec2
Sandy Figueroa <13713107+sandyfzu@users.noreply.github.com>
2024-11-07 15:00:53 -0500

    fix: fixed a top level await issue in the popup

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -4,7 +4,6 @@ import { sendMessage } from 'webext-bridge/popup';
 import { ActivateDeactivateOptions } from './pages/activate-deactivate-options';
 import { Channels, ChannelTargets } from '@/Globals';
 import type { WPRDetections } from '@/Types';
-let [currentTab] = await chrome.tabs.query({ active: true, currentWindow: true });
 const isLoading = Symbol('isLoading');
 const isError = Symbol('isError');
 const isOk = Symbol('isOk');
@@ -15,34 +14,37 @@ function App() {
   const [wprDetections, setWprDetections] = useState<WPRDetections | undefined>(undefined);
   const [tabUrl, setTabUrl] = useState<string | undefined>(undefined);
   useEffect(() => {
-    if (!currentTab.url?.startsWith('http')) {
-      setFetchState(isIncompatible);
-      return;
-    }
-    if (!currentTab || !currentTab.id) {
-      setFetchState(isError);
-      return;
-    }
-    setTabUrl(currentTab.url!);
-    const wprData = sendMessage(
-      Channels.getWPRDetections,
-      {},
-      ChannelTargets.contentScript + `@${currentTab.id}`
-    );
-    wprData
-      .then((data) => {
-        console.log(data);
-        if (!data) {
-          setFetchState(isError);
-        } else {
-          const detections = data as unknown as WPRDetections;
-          setWprDetections(detections);
-          setFetchState(isOk);
-        }
-      })
-      .catch(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+      const currentTab = tabs[0];
+      if (!currentTab || !currentTab.id) {
         setFetchState(isError);
-      });
+        return;
+      }
+      if (!currentTab.url?.startsWith('http')) {
+        setFetchState(isIncompatible);
+        return;
+      }
+      setTabUrl(currentTab.url!);
+      const wprData = sendMessage(
+        Channels.getWPRDetections,
+        {},
+        ChannelTargets.contentScript + `@${currentTab.id}`
+      );
+      wprData
+        .then((data) => {
+          console.log(data);
+          if (!data) {
+            setFetchState(isError);
+          } else {
+            const detections = data as unknown as WPRDetections;
+            setWprDetections(detections);
+            setFetchState(isOk);
+          }
+        })
+        .catch(() => {
+          setFetchState(isError);
+        });
+    });
   }, []);
 
   if (fetchState === isLoading || fetchState === isIncompatible) {
