1e4fe92a9ffec6cc8604b34376beae866904a53a
Sandy Figueroa <13713107+sandyfzu@users.noreply.github.com>
2024-11-08 15:33:16 -0500

    chore: moved known conflicts content script to isolatedWorld content script

--- entrypoints/isolatedWord.content.ts
+++ entrypoints/isolatedWord.content.ts
@@ -1,5 +1,6 @@
 import { devToolsContentScript } from '@/content-scripts/devtoolsContentScript';
 import { pageImprovements } from '@/content-scripts/improvements';
+import { knownConflictsContentScript } from '@/content-scripts/known-conflicts';
 import { sideBySideContentScript } from '@/content-scripts/sideBySide';
 import { MessageBride } from '@/Types';
 import { onMessage, sendMessage } from 'webext-bridge/content-script';
@@ -16,5 +17,6 @@ export default defineContentScript({
     devToolsContentScript(cxt, messageBridge);
     sideBySideContentScript(cxt, messageBridge);
     pageImprovements();
+    knownConflictsContentScript();
   }
 });
